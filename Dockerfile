e0c205656c7225663bc008eb3766f1f837d23e42
saracampss <s.paula1906@gmail.com>
2020-11-11 23:10:12 -0300

    #10-Finalizando docker com o banco de dados
    Co-authored-by: Giovana Vitor Dionisio giovanadionisio.gd@gmail.com

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.7-buster
-
-WORKDIR /meuchuchu
-COPY requirements.txt .
-
-RUN pip3 install -r requirements.txt
-RUN apt update
-
-CMD python3 app.py
---
+++ api/app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_object("app.config.Config")
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email, name):
+        self.email = email
+        self.name = name
+
+
+@app.route("/")
+def hello_world():
+    return jsonify(hello="world")
