fe6e2d42994ef55bdc4d62f9ac25a3c0635ce63f
dani_so_o <dannysoaresdeoliveira@gmail.com>
2020-11-22 15:41:54 -0300

    3-Criando usuario e petodos de listar e deletar

--- api/app/controller/user.py
+++ api/app/controller/user.py
@@ -1,41 +1,40 @@
 from flask import Blueprint, request, jsonify, current_app
 from app.model.tables import User
 from app.schemas.serealizer import UserSchema
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bp_user = Blueprint('user', __name__)
-@bp_user.route('/mostrar', methods=['GET'])
-def mostrar():
-    result = User.query.all()
-    return UserSchema(many=True).jsonify(result), 200
 
 @bp_user.route('/create-user', methods=['POST'])
 def register():
     us = UserSchema()
 
-    user, error = us.load(request.json)
+    user = us.load(request.json)
 
-    if error:
-        return jsonify(error), 401
+    pw_hash = generate_password_hash( user.password.encode('utf-8'), method='pbkdf2:sha512' , salt_length = 8)
+    user.password = pw_hash
 
-    user.gen_hash()
 
     current_app.db.session.add(user)
     current_app.db.session.commit()
 
     return us.jsonify(user), 201
 
-#@bp_user.route('/create-user', methods=['POST'])
-#def register():
-#    us = UserSchema()
-#
-#    user, error = us.load(request.json)
-#
-#    if error:
-#        return jsonify(error), 401
-#
-#    user.gen_hash()
-#
-#    current_app.db.session.add(user)
-#    current_app.db.session.commit()
-#
-#    return us.jsonify(user), 201
+@bp_user.route('/mostrar', methods=['GET'])
+def mostrar():
+    result = User.query.all()
+    return UserSchema(many=True).jsonify(result), 200
+
+@bp_user.route('/modificar_user/<identificador>',methods=['POST'])
+def modificar(identificador):
+    bs = UserSchema()
+    query = User.query.filter(User.id == identificador)
+    query.update(request.json)
+    current_app.db.session.commit()
+    return bs.jsonify(query.first())
+
+@bp_user.route('/delete_user/<identificador>', methods=['GET'])
+def deletar(identificador):
+    User.query.filter(User.id == identificador).delete()
+    current_app.db.session.commit()
+    return jsonify('Deletado!!!')
--- api/app/schemas/serealizer.py
+++ api/app/schemas/serealizer.py
@@ -19,12 +19,4 @@ class UserSchema(ma.ModelSchema):
     email = fields.Email(required=True)
     celular = fields.Str(required=True)
     tipo_de_user = fields.Bool(required=True)
-
-"""
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(320), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-    celular = db.Column(db.String(11), nullable=False) 
-    tipo_de_user = db.Column(db.Boolean, nullable=False)
-"""
+    image = fields.Str(required=True)
